1929473beef3e1005b2a95a56d1806bfb0f55e9d
Karikari1234 <naafizrahman1@gmail.com>
2019-05-02 17:46:40 +0600

    ride prototype made

---
+++ app/src/main/java/com/example/iutilities/share_ride.kt
@@ -0,0 +1,12 @@
+package com.example.iutilities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class share_ride : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_ride)
+    }
+}

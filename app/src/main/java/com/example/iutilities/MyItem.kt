6fadd60d7b75d3fcc9c6ff0fc127af4e5fc94caa
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2019-05-29 12:39:57 +0600

    MyProfile: Add templates for item, ride, food and tuitions deletion.

---
+++ app/src/main/java/com/example/iutilities/MyItem.kt
@@ -0,0 +1,12 @@
+package com.example.iutilities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyItem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_item)
+    }
+}
